6b02f05a2663f8dea6f5de1e7ffc7664f20e2b2e
BerkeAltiparmak <altiparmak.berke@gmail.com>
2023-02-17 21:41:33 -0500

    Read temperature from DHT11 into ESP8266

---
+++ ESP8266_temp/ESP8266_temp.ino
@@ -0,0 +1,28 @@
+//#include <dht.h>
+#include "DHT.h"
+#define DHTTYPE DHT11
+uint8_t DHTPin = D8; 
+               
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);  
+
+float temperature;
+float humidity;
+
+void setup() {
+  pinMode(DHTPin, INPUT);
+  dht.begin(); 
+  Serial.begin(115200);     
+}
+void loop() {
+  temperature = dht.readTemperature(); // Gets the values of the temperature
+  humidity = dht.readHumidity(); // Gets the values of the humidity
+  Serial.print("Temperature = ");
+  Serial.println(temperature);
+  //int temp = analogRead(pinTemp);    //Read the analog pin
+  //temp = temp * 0.48828125;   // convert output (mv) to readable celcius
+  //Serial.print("Temperature: ");
+  //Serial.print(temp);
+  //Serial.println("C");  //print the temperature status
+  delay(1000);  
+}
